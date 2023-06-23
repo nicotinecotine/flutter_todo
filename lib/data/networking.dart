7b1c6aff3332bfc268f9be3f9a96a37d4ce1ee6f
nicotinecotine <nikatinnek@gmail.com>
2023-06-24 02:55:15 +0300

    linter stuff

--- lib/data/networking.dart
+++ lib/data/networking.dart
@@ -1,18 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_todo/models/task_model.dart';
 
-final String baseUrl = 'https://beta.mrdekk.ru/todobackend';
-final String token = '';
-final int revision = 0;
+const String baseUrl = 'https://beta.mrdekk.ru/todobackend';
+const String token = '';
+int revision = 0;
 Dio dioInstance = Dio();
 
 Future<List<Task>> getTasksList() async {
-  dioInstance.options.headers['Authorization'] = "Bearer ${token}";
+  dioInstance.options.headers['Authorization'] = "Bearer $token";
   return ([]);
 }
 
 Future<void> updateTasksList() async {
-  dioInstance.options.headers['Authorization'] = "Bearer ${token}";
+  dioInstance.options.headers['Authorization'] = "Bearer $token";
   return Future.delayed(
     const Duration(
       seconds: 1,
